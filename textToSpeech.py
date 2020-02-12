c041d17acc550f121ea319765c416255f25c50f9
Fendy <fendy@aritonang.web.id>
2020-02-12 06:12:07 +0000

    Added text to speech

---
+++ textToSpeech.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+from subprocess import call
+
+mytext = 'Welcome to geeksforgeeks!'
+
+# Language in which you want to convert 
+language = 'en'
+
+# Passing the text and language to the engine,
+# here we have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# Saving the converted audio in a mp3 file named
+# welcome
+myobj.save("welcome.wav")
+
+call(["aplay", "welcome.wav"])
